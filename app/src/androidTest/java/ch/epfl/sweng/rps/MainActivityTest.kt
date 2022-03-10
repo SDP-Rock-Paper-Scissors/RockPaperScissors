8f918e64c46be9fdf453a0b6fab47a693e884778
Adam Narozniak <narozniak.adam4@gmail.com>
2022-03-10 17:56:51 +0100

    test: add radio button tests

--- app/src/androidTest/java/ch/epfl/sweng/rps/MainActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sweng/rps/MainActivityTest.kt
@@ -5,7 +5,6 @@ import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
