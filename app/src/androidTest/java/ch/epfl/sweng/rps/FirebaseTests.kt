d27465c7f03006561aa395af4b41ef4431b077ad
Gaëtan S <blaxoujunior@gmail.com>
2022-03-14 13:47:51 +0100

    fix: comment out wip test

--- app/src/androidTest/java/ch/epfl/sweng/rps/FirebaseTests.kt
+++ app/src/androidTest/java/ch/epfl/sweng/rps/FirebaseTests.kt
@@ -1,24 +1,11 @@
 package ch.epfl.sweng.rps
 
-import android.util.Log
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import ch.epfl.sweng.rps.db.Env
-import ch.epfl.sweng.rps.db.FirebaseRepository
-import com.google.firebase.FirebaseApp
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runTest
-import org.junit.After
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
-
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+/*
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class FirebaseTests {
@@ -37,10 +24,10 @@ class FirebaseTests {
         Log.d("FirebaseTests", "tearDown done")
     }
 
-    // @Test
+    @Test
     fun testCreateUser() {
         runTest {
             db.createUser("Gaëtan S.", "gaetan.schwartz@epfl.ch")
         }
     }
-}
+}*/
