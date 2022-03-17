e384b2d054ee9d430c56f93822428db611d30cbf
leonardo <leonardo.pennino98@gmail.com>
2022-03-18 00:55:47 +0100

    Fixed statitics fragment test

--- app/src/androidTest/java/ch/epfl/sweng/rps/StatisticsFragmentTest.kt
+++ app/src/androidTest/java/ch/epfl/sweng/rps/StatisticsFragmentTest.kt
@@ -1,9 +1,11 @@
 package ch.epfl.sweng.rps
 
 import android.view.Gravity
-
+import android.view.View
 import androidx.test.espresso.Espresso
-
+import androidx.test.espresso.PerformException
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.contrib.DrawerActions
@@ -11,10 +13,13 @@ import androidx.test.espresso.contrib.DrawerMatchers
 import androidx.test.espresso.contrib.NavigationViewActions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.util.HumanReadables
+import androidx.test.espresso.util.TreeIterables
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
+import java.util.concurrent.TimeoutException
 
 
 class StatisticsFragmentTest {
@@ -40,10 +45,46 @@ class StatisticsFragmentTest {
 
         Espresso.onView(withId(R.id.test_for_stats_row)).perform(click())
 
+        Thread.sleep(3000)
         Espresso.onView(ViewMatchers.withId(R.id.fragment_match_details))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
 
         }
+
+    /** Perform action of waiting for a specific view id.  */
+    fun waitId(viewId: Int, millis: Long): ViewAction? {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isRoot()
+            }
+
+            override fun getDescription(): String {
+                return "wait for a specific view with id <$viewId> during $millis millis."
+            }
+
+            override fun perform(uiController: UiController, view: View?) {
+                uiController.loopMainThreadUntilIdle()
+                val startTime = System.currentTimeMillis()
+                val endTime = startTime + millis
+                val viewMatcher: Matcher<View> = withId(viewId)
+                do {
+                    for (child in TreeIterables.breadthFirstViewTraversal(view)) {
+                        // found view with required ID
+                        if (viewMatcher.matches(child)) {
+                            return
+                        }
+                    }
+                    uiController.loopMainThreadForAtLeast(50)
+                } while (System.currentTimeMillis() < endTime)
+                throw PerformException.Builder()
+                    .withActionDescription(this.description)
+                    .withViewDescription(HumanReadables.describe(view))
+                    .withCause(TimeoutException())
+                    .build()
+            }
+        }
     }
+
+}
 
