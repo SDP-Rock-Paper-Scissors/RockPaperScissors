4da4b9b3554d1380f769a16b67532103b9d59624
bhvrgav <bhargav.venugopal@epfl.ch>
2022-03-24 21:48:47 +0100

    Revert "Get Up To Date"
    This reverts commit e703193a4dcd4de2af31e93c79f6887f2e3c0939.

--- app/src/main/java/ch/epfl/sweng/rps/ui/game/GameFragment.kt
+++
@@ -1,6 +0,0 @@
-package ch.epfl.sweng.rps.ui.game
-
-import androidx.fragment.app.Fragment
-import ch.epfl.sweng.rps.R
-
-class GameFragment : Fragment(R.layout.fragment_game)
--- app/src/main/java/ch/epfl/sweng/rps/ui/home/HomeFragment.kt
+++ app/src/main/java/ch/epfl/sweng/rps/ui/home/HomeFragment.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation
-import ch.epfl.sweng.rps.R
 import ch.epfl.sweng.rps.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -24,14 +23,13 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this)[HomeViewModel::class.java]
+            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        _binding!!.buttonPlayOneOfflineGame.setOnClickListener { view: View ->
-            Navigation.findNavController(view).navigate(R.id.gameFragment)
-        }
+        val root: View = binding.root
 
-        return binding.root
+
+        return root
     }
 
     override fun onDestroyView() {
