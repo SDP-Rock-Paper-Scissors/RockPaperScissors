deb8d28c6576bdbb4a732c9c5abb8ae44be72640
panadax <panadax@outlook.com>
2022-03-25 03:41:50 +0100

    link to db for stats page

--- app/src/main/java/ch/epfl/sweng/rps/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/ch/epfl/sweng/rps/ui/statistics/StatisticsFragment.kt
@@ -1,7 +1,9 @@
 package ch.epfl.sweng.rps.ui.statistics
 
+import android.content.ContentValues.TAG
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +12,11 @@ import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
 import ch.epfl.sweng.rps.R
 import ch.epfl.sweng.rps.databinding.FragmentStatisticsBinding
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class StatisticsFragment : Fragment() {
@@ -29,7 +36,8 @@ class StatisticsFragment : Fragment() {
         )
         val newView = inflater.inflate(R.layout.fragment_statistics, container, false)
         val modeSpinner = newView.findViewById(R.id.modeSelect) as Spinner
-        val adapter = ArrayAdapter(this.requireActivity(), android.R.layout.simple_spinner_item, values)
+        val adapter =
+            ArrayAdapter(this.requireActivity(), android.R.layout.simple_spinner_item, values)
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
         modeSpinner.adapter = adapter
         modeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
@@ -59,19 +67,42 @@ class StatisticsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        addPersonalStats(view,"b9d5384-9f1f-11ec-b909-0242ac120002","2022-03-09","test","12", "4:8")
+        readStatsData("wwHIHq1zrZADupznqThr", object : FirebaseCallBack {
+            override fun onCallBack(allStatsResult: MutableList<List<String>>) {
+                for (data in allStatsResult){
+                    addPersonalStats(view, data[0],data[1],data[2],data[3],data[4])
+
+                }
+            }
+        })
+        
+        addPersonalStats(
+            view,
+            "b9d5384-9f1f-11ec-b909-0242ac120002",
+            "2022-03-09",
+            "test",
+            "12",
+            "4:8"
+        )
+
 
     }
 
 
-
-
-    private fun addPersonalStats(view: View, uuid: String, date:String, opponent:String, mode:String, score: String ) {
+    private fun addPersonalStats(
+        view: View,
+        uuid: String,
+        date: String,
+        opponent: String,
+        mode: String,
+        score: String
+    ) {
         val sizeInDp = 5
         val statsTable = view.findViewById<TableLayout>(R.id.statsTable)
         val row = TableRow(activity)
         row.setBackgroundColor(
-            Color.parseColor("#F0F7F7"))
+            Color.parseColor("#F0F7F7")
+        )
         val scale = resources.displayMetrics.density
         val dpAsPixels = (sizeInDp * scale + 0.5f)
 
@@ -87,9 +118,9 @@ class StatisticsFragment : Fragment() {
             val matchDetailFragment = MatchDetails()
             val bundle = Bundle()
             val transaction = requireActivity().supportFragmentManager.beginTransaction()
-            bundle.putString("uuid",uuid)
-            matchDetailFragment.arguments =  bundle
-            transaction.replace(R.id.fragment_statistics,matchDetailFragment)
+            bundle.putString("uuid", uuid)
+            matchDetailFragment.arguments = bundle
+            transaction.replace(R.id.fragment_statistics, matchDetailFragment)
             transaction.addToBackStack(null)
             transaction.commit()
 
@@ -123,8 +154,93 @@ class StatisticsFragment : Fragment() {
         statsTable.addView(row)
 
     }
+
+
+    private fun readStatsData(userID: String, firebaseCallBack: FirebaseCallBack) {
+        val usersDB = Firebase.firestore.collection("users")
+        val matchesDB = Firebase.firestore.collection("matches")
+        val allStatsResult: MutableList<List<String>> = ArrayList()
+        val statsResult: MutableList<String> = ArrayList()
+        //return list format: match_uuid, date,opponent, mode, score
+        usersDB.document(userID).get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+
+                    var matchHistory: List<String> = ArrayList()
+                    matchHistory = document.data?.get("match_history") as List<String>
+                    for (matchUUID in matchHistory) {
+                        //retrieve match_uuid for next detail page query
+                        statsResult.add(matchUUID)
+                        matchesDB.document(matchUUID).get()
+                            .addOnSuccessListener { doc ->
+                                if (doc != null) {
+                                    //retrieve first component: date
+                                    val matchDate: Timestamp =
+                                        doc.data?.get("match_date") as Timestamp
+                                    val matchDateFormat =
+                                        SimpleDateFormat("yyyy-MM-dd").format(matchDate.toDate())
+                                    statsResult.add(matchDateFormat)
+                                    //retrieve second component: opponent
+                                    val details =
+                                        doc.data?.get("details") as HashMap<String, String>
+                                    val players = details.keys
+                                    for (playerID in players) {
+                                        if (playerID != userID) {
+                                            statsResult.add(playerID)
+                                        }
+                                    }
+                                    //retrieve third component: mode
+                                    val mode = doc.data?.get("mode")
+                                    statsResult.add(mode as String)
+                                    //retrieve fourth component: score
+                                    val overallScore =
+                                        doc.data?.get("overall_score") as HashMap<String, String>
+                                    val playerScore = overallScore[userID]
+                                    val opponentScore = overallScore[statsResult[2]]
+                                    val score = "$playerScore - $opponentScore"
+                                    statsResult.add(score)
+                                    allStatsResult.add(statsResult)
+                                    firebaseCallBack.onCallBack(allStatsResult)
+
+                                } else {
+                                    Log.d(TAG, "No such document")
+                                }
+                            }
+                    }
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
+
+/*
+        usersDB.document(statsResult[2]).get()
+            .addOnSuccessListener { result ->
+                val opponentName = result.data?.get("user_name")
+                println(opponentName)
+                statsResult.add(opponentName as String)
+                println(statsResult)
+            }
+            */
+
+
+
+    }
+
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+
+    private interface FirebaseCallBack {
+        fun onCallBack(list: MutableList<List<String>>) {
+
+        }
     }
 }
