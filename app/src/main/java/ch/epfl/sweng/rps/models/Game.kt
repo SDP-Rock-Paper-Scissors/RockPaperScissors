921816d6bd85b01fc968a16a65332a36de9ae92e
Adam Narozniak <narozniak.adam4@gmail.com>
2022-03-31 20:42:53 +0200

    feat: adjust the models to the usability It wasn't my part though I had to do that in order to be able to have match playing with computer

--- app/src/main/java/ch/epfl/sweng/rps/models/Game.kt
+++ app/src/main/java/ch/epfl/sweng/rps/models/Game.kt
@@ -1,20 +1,30 @@
 package ch.epfl.sweng.rps.models
 
+import com.google.firebase.Timestamp
+import java.util.*
+
 
 /**
  * Represents a game.
- * @property uid Unique ID of the game
- * @property players The
+ * @property gameId Unique ID of the game
+ * @property uid user id.
+ * @property players list of players id and information if it is a bot
  * @property mode The time limit, if any, in seconds.
  */
 data class Game(
+    val gameId: String,
     val uid: String,
     val players: List<Uid>,
-    val mode: Mode
+    val mode: Mode,
+    var roundsList: MutableList<Round>?
 ) {
+    private var uidToScore: MutableMap<String, Int> = mutableMapOf()
+
     init {
-        assert(players.size == mode.playerCount) { "The number of players doesn't coincide with the gameMode !" }
+        assert((players.size + 1)== mode.playerCount) { "The number of players doesn't coincide with the gameMode !" }
+        initializeScoreList()
     }
+
 
     /**
      * Represents a game mode.
@@ -32,4 +42,29 @@ data class Game(
         val uid: String,
         val isComputer: Boolean = false
     )
+
+    private fun initializeScoreList(){
+        this.uidToScore = mutableMapOf()
+        for (id in players){
+            uidToScore[id.uid] = 0
+        }
+        uidToScore[uid] = 0
+    }
+
+    /**
+     * After a finished round (at time the hands of each player is known) the round is added.
+     */
+    fun addRound(uid:String, userHand: Hand, opponentsHands: Map<String, Hand>){
+        val roundResult = mutableMapOf<String, Hand>()
+        for ((key, value) in opponentsHands.entries){
+            roundResult[key] = value
+        }
+        roundResult[uid] = userHand
+        if (roundsList == null){
+            roundsList = mutableListOf(Round(roundResult, Timestamp(Date()), "toChangeToRealUid"))
+        }
+        else{
+            roundsList?.add(Round(roundResult, Timestamp(Date()), "toChangeToRealUid"))
+        }
+    }
 }
--- app/src/main/java/ch/epfl/sweng/rps/models/Round.kt
+++ app/src/main/java/ch/epfl/sweng/rps/models/Round.kt
@@ -6,14 +6,19 @@ import com.google.firebase.Timestamp
 data class Round(
     val hands: Map<String, Hand>,
     val timestamp: Timestamp,
-    val uid: String,
+    val roundId: String,
 ) {
+    private var userToPointsList:  List<Score> = listOf()
+    private var _winnerId: String = ""
+    val winnerId: String
+        get() = _winnerId
+
     fun computeScores(pointSystem: PointSystem = PointSystem.DEFAULT): List<Score> {
-        val points = hashMapOf<String, Int>()
+        val userToPoints: HashMap<String, Int> = HashMap()
         for ((uid, hand) in hands) {
             for ((uid2, hand2) in hands) {
                 if (uid != uid2) {
-                    points[uid] = (points[uid] ?: 0) + when (hand vs hand2) {
+                    userToPoints[uid] = (userToPoints[uid] ?: 0) + when (hand vs hand2) {
                         Result.WIN -> pointSystem.win
                         Result.LOSE -> pointSystem.lose
                         Result.DRAW -> pointSystem.draw
@@ -21,11 +26,25 @@ data class Round(
                 }
             }
         }
-        return points.map { Score(it.key, it.value) }.sortedByDescending { it.score }
+        userToPointsList = userToPoints.map { Score(it.key, it.value) }.sortedByDescending { it.score }
+        determineWinner()
+        return userToPointsList
     }
 
     data class Score(
         val uid: String,
         val score: Int
     )
+
+    /**
+     * Simplified version for 2 people.
+     */
+    private fun determineWinner(){
+        if (userToPointsList.first().score == 1)
+            _winnerId = userToPointsList.first().uid
+        else{
+            _winnerId = ""
+        }
+    }
+
 }
