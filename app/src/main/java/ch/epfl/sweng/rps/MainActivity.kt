1bc3d44215c0e1f868b0fa9299d9f90fdb4ac211
Silvio Müller <silvio.muller@epfl.ch>
2022-04-05 12:18:40 +0200

    fix: remove bottom nav view for game fragment and camera fragment

--- app/src/main/java/ch/epfl/sweng/rps/MainActivity.kt
+++ app/src/main/java/ch/epfl/sweng/rps/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -32,15 +33,31 @@ class MainActivity : AppCompatActivity() {
 
             binding = ActivityMainBinding.inflate(layoutInflater)
             setContentView(binding.root)
+            setupNav()
 
-            val navView: BottomNavigationView = binding.navView
-
-            val navController = findNavController(R.id.nav_host_fragment_activity_main)
-
-            navView.setupWithNavController(navController)
         }
 
     fun getUserDetails() : User{
         return currentUser
+    }
+
+    private fun setupNav() {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navView: BottomNavigationView = binding.navView
+        navView.setupWithNavController(navController)
+
+        //removes botttomNavView for specified fragments.
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.cameraFragment -> setBottomNavigationVisibility(View.GONE)
+                R.id.gameFragment -> setBottomNavigationVisibility(View.GONE)
+                else -> setBottomNavigationVisibility(View.VISIBLE)
+            }
+        }
+    }
+
+    fun setBottomNavigationVisibility(visibility: Int) {
+        // get the reference of the bottomNavigationView and set the visibility.
+        binding.navView.visibility = visibility
     }
 }
