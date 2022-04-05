dabf4eb8abab2e6aee803e742aa734d9be508226
Silvio Müller <silvio.muller@epfl.ch>
2022-04-05 10:43:58 +0200

    feat: Go back to last fragment if permissions are denied

--- app/src/main/java/ch/epfl/sweng/rps/ui/camera/CameraFragment.kt
+++ app/src/main/java/ch/epfl/sweng/rps/ui/camera/CameraFragment.kt
@@ -9,6 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation
+import ch.epfl.sweng.rps.R
 import ch.epfl.sweng.rps.databinding.FragmentCameraBinding
 
 
@@ -28,6 +31,7 @@ class CameraFragment : Fragment() {
             // permission granted continue the normal workflow of app
             Log.i("DEBUG", "permission granted")
         } else {
+            navigateBackHome()
             // if permission denied then check whether never ask
             // again is selected or not by making use of
             // !ActivityCompat.shouldShowRequestPermissionRationale(
@@ -53,5 +57,12 @@ class CameraFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun navigateBackHome() {
+        lifecycleScope.launchWhenStarted {
+            Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_activity_main).navigate(
+                CameraFragmentDirections.actionCameraFragmentToNavHome())
+        }
     }
 }
