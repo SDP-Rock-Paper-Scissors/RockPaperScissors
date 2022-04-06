f7c0341dd0709a42865a80340c371b29f09358cf
Gaëtan S <blaxoujunior@gmail.com>
2022-04-06 15:25:10 +0200

    fix: profile fragment test

--- app/src/androidTest/java/ch/epfl/sweng/rps/ProfileFragmentTest.kt
+++ app/src/androidTest/java/ch/epfl/sweng/rps/ProfileFragmentTest.kt
@@ -8,33 +8,44 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import org.junit.Before
+import androidx.test.platform.app.InstrumentationRegistry
+import ch.epfl.sweng.rps.models.User
 import org.junit.Rule
 import org.junit.Test
 
 
 class ProfileFragmentTest {
-    private val i: Intent = Intent()
-    private val b: Bundle = Bundle()
-    private val data = mapOf<String, String>(
-        "email" to "asd@gmail.com", "display_name" to "asdino", "uid" to "123", "privacy" to "None"
-    )
 
-    @Before
-    fun setUpIntent() {
-        data.forEach { b.putString(it.key, it.value) }
-        i.putExtra("User", b)
+    private val bundle = run {
+        val b: Bundle = Bundle()
+        val data = mapOf(
+            "email" to "asd@gmail.com",
+            "display_name" to "asdino",
+            "uid" to "123",
+            "privacy" to User.Privacy.PUBLIC.toString()
+        )
+        data.forEach { (k, v) -> b.putString(k, v) }
+        b
+    }
+
+
+    private fun createIntent(): Intent {
+        val i: Intent = Intent(
+            InstrumentationRegistry.getInstrumentation().targetContext,
+            MainActivity::class.java
+        )
+        i.putExtra("User", bundle)
+        return i
     }
 
     @get:Rule
-    val testRule = ActivityScenarioRule(MainActivity::class.java)
+    val testRule = ActivityScenarioRule<MainActivity>(createIntent())
 
     @Test
     fun testFields() {
-
         onView(withId(R.id.nav_profile)).perform(click())
-        onView(withId(R.id.TextDisplayName)).check(matches(withText(b.getString("display_name"))))
-        onView(withId(R.id.TextEmail)).check(matches(withText(b.getString("email"))))
-        onView(withId(R.id.TextPrivacy)).check(matches(withText(b.getString("privacy"))))
+        onView(withId(R.id.TextDisplayName)).check(matches(withText(bundle.getString("display_name"))))
+        onView(withId(R.id.TextEmail)).check(matches(withText(bundle.getString("email"))))
+        onView(withId(R.id.TextPrivacy)).check(matches(withText(bundle.getString("privacy"))))
     }
 }
