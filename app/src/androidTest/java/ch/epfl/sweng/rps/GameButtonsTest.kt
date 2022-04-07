c282620abe0333ec3dbb27a02e83587b9c3fabf5
Adam Narozniak <narozniak.adam4@gmail.com>
2022-04-07 21:06:18 +0200

    test: game simulation (tough no support for coroutines, delay temporarily commented)

--- app/src/androidTest/java/ch/epfl/sweng/rps/GameButtonsTest.kt
+++ app/src/androidTest/java/ch/epfl/sweng/rps/GameButtonsTest.kt
@@ -3,49 +3,46 @@ package ch.epfl.sweng.rps
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isChecked
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.anyOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-// temporarily stop the tests, new behaviour needed
-// their state can't be checked, new screen is loaded
-//@RunWith(AndroidJUnit4::class)
-//@SmallTest
-//class GameButtonsTest {
-//    @get:Rule
-//    val testRule = ActivityScenarioRule(MainActivity::class.java)
-//    @Test
-//    fun pressedRock(){
-//        checkPressedButton(R.id.rockRB)
-//    }
-//    @Test
-//    fun pressedPaper(){
-//        checkPressedButton(R.id.paperRB)
-//    }
-//    @Test
-//    fun pressedScissors() {
-//        checkPressedButton(R.id.scissorsRB)
-//    }
-//    private fun checkPressedButton(radioButtonId: Int){
-//        onView(withId(R.id.button_play_one_offline_game)).perform(click())
-//        onView(withId(radioButtonId)).perform(click())
-//        onView(withId(radioButtonId)).check(matches(isChecked()))
-//    }
-//    @Test
-//    fun pressedRockPaperRock(){
-//        onView(withId(R.id.button_play_one_offline_game)).perform(click())
-//        onView(withId(R.id.rockRB)).perform(click())
-//        onView(withId(R.id.paperRB)).perform(click())
-//        onView(withId(R.id.rockRB)).perform(click())
-//        onView(withId(R.id.rockRB)).check(matches(isChecked()))
-//        onView(withId(R.id.paperRB)).check(matches(not(isChecked())))
-//        onView(withId(R.id.scissorsRB)).check(matches(not(isChecked())))
-//    }
-//
-//
-//}
+
+
+@RunWith(AndroidJUnit4::class)
+class GameButtonsTest {
+    @get:Rule
+    val testRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun pressedRock() {
+        checkPressedButton(R.id.rockRB)
+    }
+
+    @Test
+    fun pressedPaper() {
+        checkPressedButton(R.id.paperRB)
+    }
+
+    @Test
+    fun pressedScissors() {
+        checkPressedButton(R.id.scissorsRB)
+    }
+
+    private fun checkPressedButton(radioButtonId: Int) {
+        onView(withId(R.id.button_play_1_games_offline)).perform(click())
+        onView(withId(radioButtonId)).perform(click())
+        onView(
+            anyOf(
+                withText(R.string.win),
+                withText(R.string.lose),
+                withText(R.string.draw)
+            )
+        ).check(matches(isDisplayed()))
+
+    }
+}
 
