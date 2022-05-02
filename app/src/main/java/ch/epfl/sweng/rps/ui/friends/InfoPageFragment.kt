70e71a2735406873a45ade71cd925008fc2e98fc
Gaëtan S <blaxoujunior@gmail.com>
2022-05-02 19:30:18 +0200

    fix imports

--- app/src/main/java/ch/epfl/sweng/rps/ui/friends/InfoPageFragment.kt
+++ app/src/main/java/ch/epfl/sweng/rps/ui/friends/InfoPageFragment.kt
@@ -1,13 +1,13 @@
 package ch.epfl.sweng.rps.ui.friends
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ch.epfl.sweng.rps.R
 
