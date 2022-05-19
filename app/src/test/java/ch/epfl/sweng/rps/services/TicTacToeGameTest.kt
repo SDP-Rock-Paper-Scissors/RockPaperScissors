995d59ff6ff3116e2ad4d0421232fa2c279e4a66
leonardo <leonardo.pennino98@gmail.com>
2022-05-19 23:06:58 +0200

    Fixed tests that used old interface

--- app/src/test/java/ch/epfl/sweng/rps/services/TicTacToeGameTest.kt
+++ app/src/test/java/ch/epfl/sweng/rps/services/TicTacToeGameTest.kt
@@ -9,11 +9,11 @@ class TicTacToeGameTest {
     lateinit var ticTacToe:TicTacToeGame
     @BeforeEach
     fun setup(){
-        ticTacToe = object : TicTacToeGame(){
-            override fun putChoice(move: MOVES, square: Int) {
+        ticTacToe = object : TicTacToeGame(MOVES.CIRCLE){
+            override fun putChoice( square: Int) {
                 val cell = square % 3
                 val row = (square / 3)
-                matrix[row][cell] = move
+                matrix[row][cell] = player
             }
 
             override fun gameOver(winner: MOVES) {
@@ -24,38 +24,38 @@ class TicTacToeGameTest {
     @Test
     fun TTTcorrectlycalculatesWinRow(){
         assert(ticTacToe.gameRunning)
-        ticTacToe.putChoice(TicTacToeGame.MOVES.CIRCLE, 0)
-        ticTacToe.putChoice(TicTacToeGame.MOVES.CIRCLE, 1)
-        ticTacToe.putChoice(TicTacToeGame.MOVES.CIRCLE, 2)
+        ticTacToe.putChoice( 0)
+        ticTacToe.putChoice( 1)
+        ticTacToe.putChoice( 2)
         ticTacToe.calculate()
         assert(!ticTacToe.gameRunning)
     }
     @Test
     fun TTTcorrectlycalculatesWinCol(){
         assert(ticTacToe.gameRunning)
-        ticTacToe.putChoice(TicTacToeGame.MOVES.CIRCLE, 0)
-        ticTacToe.putChoice(TicTacToeGame.MOVES.CIRCLE, 3)
-        ticTacToe.putChoice(TicTacToeGame.MOVES.CIRCLE, 6)
+        ticTacToe.putChoice(0)
+        ticTacToe.putChoice(3)
+        ticTacToe.putChoice(6)
         ticTacToe.calculate()
         assert(!ticTacToe.gameRunning)
     }
     @Test
     fun TTTcorrectlycalculatesWinDiag1(){
         assert(ticTacToe.gameRunning)
-        ticTacToe.putChoice(TicTacToeGame.MOVES.CIRCLE, 2)
-        ticTacToe.putChoice(TicTacToeGame.MOVES.CIRCLE, 4)
-        ticTacToe.putChoice(TicTacToeGame.MOVES.CIRCLE, 6)
+        ticTacToe.putChoice(2)
+        ticTacToe.putChoice(4)
+        ticTacToe.putChoice(6)
         ticTacToe.calculate()
         assert(!ticTacToe.gameRunning)
     }
     @Test
     fun TTTcorrectlycalculatesWinDiag2(){
         assert(ticTacToe.gameRunning)
-        ticTacToe.putChoice(TicTacToeGame.MOVES.CIRCLE, 0)
+        ticTacToe.putChoice(0)
         ticTacToe.calculate()
         assert(ticTacToe.gameRunning)
-        ticTacToe.putChoice(TicTacToeGame.MOVES.CIRCLE, 4)
-        ticTacToe.putChoice(TicTacToeGame.MOVES.CIRCLE, 8)
+        ticTacToe.putChoice(4)
+        ticTacToe.putChoice(8)
         ticTacToe.calculate()
         assert(!ticTacToe.gameRunning)
     }
--- app/src/test/java/ch/epfl/sweng/rps/services/TicTacToeOfflineTest.kt
+++ app/src/test/java/ch/epfl/sweng/rps/services/TicTacToeOfflineTest.kt
@@ -9,10 +9,11 @@ class TicTacToeOfflineTest {
     lateinit var tictactoe:OfflineTicTacToe
     lateinit var view: TicTacToeFragment
     var choice = Pair(0,1)
+    var player = TicTacToeGame.MOVES.CIRCLE
     @BeforeEach
     fun setup(){
         view = mockk<TicTacToeFragment>(relaxed = true)
-        tictactoe = OfflineTicTacToe(view)
+        tictactoe = OfflineTicTacToe(view,player)
         val opponent = object : TicTacToeOpponent {
             override fun makeMove(): Pair<Int, Int> {
                return choice
@@ -23,25 +24,25 @@ class TicTacToeOfflineTest {
     @Test
     fun putChoiceCorrectlyPutsChoice() {
         choice = Pair(0,1)
-        tictactoe.putChoice(TicTacToeGame.MOVES.CIRCLE, 0)
+        tictactoe.putChoice(0)
         assert(tictactoe.matrix[0][0] == TicTacToeGame.MOVES.CIRCLE)
         assert(tictactoe.matrix[0][1] == TicTacToeGame.MOVES.CROSS)
         choice = Pair(1, 2)
-        tictactoe.putChoice(TicTacToeGame.MOVES.CIRCLE, 3)
+        tictactoe.putChoice(3)
         assert(tictactoe.matrix[1][0] == TicTacToeGame.MOVES.CIRCLE)
         assert(tictactoe.matrix[1][2] == TicTacToeGame.MOVES.CROSS)
         choice = Pair(2,2)
-        tictactoe.putChoice(TicTacToeGame.MOVES.CIRCLE, 6)
+        tictactoe.putChoice(6)
         assert(tictactoe.matrix[2][0] == TicTacToeGame.MOVES.CIRCLE)
         assert(!tictactoe.gameRunning)
-        tictactoe.putChoice(TicTacToeGame.MOVES.CIRCLE,2)
+        tictactoe.putChoice(2)
         assert(tictactoe.matrix[0][2] == TicTacToeGame.MOVES.EMPTY)
     }
     @Test
     fun putChoice2TimesDoesNotChange(){
-        tictactoe.putChoice(TicTacToeGame.MOVES.CIRCLE, 0)
+        tictactoe.putChoice(0)
         assert(tictactoe.matrix[0][0] == TicTacToeGame.MOVES.CIRCLE)
-        tictactoe.putChoice(TicTacToeGame.MOVES.CROSS, 0)
+        tictactoe.putChoice(0)
         assert(tictactoe.matrix[0][0] == TicTacToeGame.MOVES.CIRCLE)
     }
 }
